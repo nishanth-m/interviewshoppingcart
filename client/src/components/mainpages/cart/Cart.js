adc82fcedfa3bb05edd40138bf2e6839561ee145
Nishanth <nishanth.belman@gmail.com>
2021-05-03 10:33:58 -0700

    tax apply functionality

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -8,15 +8,30 @@ function Cart() {
     const [cart, setCart] = state.userAPI.cart
     const [token] = state.token
     const [total, setTotal] = useState(0)
+    const [totaltax, setTotaltax] = useState(0)
 
     useEffect(() =>{
         const getTotal = () =>{
             const total = cart.reduce((prev, item) => {
-                return prev + (item.price * item.quantity)
+                return prev + (((item.price)+((item.tax===0)?0:((item.price*item.tax)/100))) * item.quantity)
             },0)
 
-            setTotal(total)
+        
+
+
+
+            setTotal(parseFloat(total).toFixed(2))
+           
         }
+        const getTotalTax = ()=>{
+            const totaltax = cart.reduce((prev, item) => {
+                return prev + ((((item.tax===0)?0:((item.price*item.tax)/100))) * item.quantity)
+            },0)
+
+            setTotaltax(parseFloat(totaltax).toFixed(2))
+
+        }
+        getTotalTax()
 
         getTotal()
 
@@ -108,8 +123,11 @@ function Cart() {
                     </div>
                 ))
             }
-
+            <div className="totaltax">
+            <h3>Total Tax: &#8377;{totaltax}</h3>
+            </div>
             <div className="total">
+                
                 <h3>Total: &#8377; {total}</h3>
             </div>
         </div>
